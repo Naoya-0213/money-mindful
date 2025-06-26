66150b44f1fece8c0d13568398e2526bc375c190
Naoya Noguchi <naoya.work0213@gmail.com>
2025-06-26 11:28:37 +0900

    変更

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -9,22 +9,31 @@ const Footer = () => {
       style={{ height: "var(--footer-height)" }}
       className="fixed bottom-0 z-50 grid w-full max-w-[480px] grid-cols-5 bg-[#D7CDBE]"
     >
+      {/* ホーム */}
       <FooterItem
         icon="/icon/home.png"
         label="ホーム"
         href="/money-mindful/home"
       />
+
+      {/* 追加 */}
       <FooterItem icon="/icon/add.png" label="追加" href="/money-mindful/add" />
+
+      {/* 合計 */}
       <FooterItem
         icon="/icon/graph.png"
         label="合計"
-        href="/money-mindful/home"
+        href="/money-mindful/total"
       />
+
+      {/* 記録（カレンダー） */}
       <FooterItem
         icon="/icon/calendar.png"
         label="記録"
         href="/money-mindful/home"
       />
+
+      {/* 設定 */}
       <FooterItem
         icon="/icon/setting.png"
         label="設定"
--- src/app/components/total/Total.tsx
+++ src/app/components/total/Total.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Total = () => {
   return (
-    <div>Total</div>
-  )
-}
+    <div className="flex w-full flex-col gap-5 p-5 text-[#795549]">
+      {/* セクションバー */}
 
-export default Total
+      {/* 合計表示 */}
+      <div className="text-3xl font-bold">
+        <h3>今週の合計</h3>
+        <p>¥20,000</p>
+      </div>
+    </div>
+  );
+};
+
+export default Total;

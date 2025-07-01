2b9c547757d90144b908af4b8dc5140ec6689654
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-01 22:18:35 +0900

    ホーム目標セクション完了

--- src/app/components/section-card/GoalCard.tsx
+++ src/app/components/section-card/GoalCard.tsx
@@ -13,7 +13,32 @@ const GoalCard = () => {
         label="目標設定icon"
       >
         {/* データ取得 */}
-        <div></div>
+        <div className="flex flex-col gap-5">
+          {/* 目的 */}
+          <div className="flex items-center gap-5">
+            <p className="flex h-auto min-h-[65px] items-center rounded-xl bg-[#F3F0EB] px-6 py-4 text-xl font-bold">
+              北海道旅行
+            </p>
+
+            <span className="min-w-[80px] text-xl font-bold">のために</span>
+          </div>
+
+          {/* 期限 */}
+          <div className="flex items-center gap-5">
+            <p className="flex h-auto min-h-[65px] items-center rounded-xl bg-[#F3F0EB] px-6 py-4 text-xl font-bold">
+              2025年9月30日
+            </p>
+            <span className="min-w-[80px] text-xl font-bold">までに</span>
+          </div>
+
+          {/* 金額 */}
+          <div className="flex items-center gap-5">
+            <p className="flex h-auto min-h-[65px] items-center rounded-xl bg-[#F3F0EB] px-6 py-4 text-xl font-bold">
+              ¥50,000
+            </p>
+            <span className="min-w-[100px] text-xl font-bold">を貯める！</span>
+          </div>
+        </div>
 
         {/* 目標編集ボタン */}
         <Link href="/money-mindful/add" className="flex w-full justify-center">

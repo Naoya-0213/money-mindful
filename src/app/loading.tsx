130b23ca2b9acd60b06c540e48d38b765a00ed99
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-12 23:01:31 +0900

    loading画面実装_7/12_23:01

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,1 +1,11 @@
 // ローディング画面用（自動で表示）
+
+const LoadingSpinner = () => {
+  return (
+    <div className="mt-20 flex justify-center">
+      <div className="h-7 w-7 animate-spin rounded-full border-2 border-[#795549] border-t-transparent" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;

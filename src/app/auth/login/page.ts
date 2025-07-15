0259e1a21b409763f55218b44142975a946ce4b2
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-15 09:48:21 +0900

    変更_7/15_9:48

--- src/app/auth/login/page.ts
+++ src/app/auth/login/page.ts
@@ -1,26 +1,16 @@
-// // ログインページ実装
-// // use client側の components/login の読み込み
-// // use server
-
-// import type { Database } from "@/types/database.types";
-// import { createClient } from "@/utils/supabase/clients";
 // import { redirect } from "next/navigation";
-// import BeforeLoginPage from '../../money-mindful/login/page';
+// import { createClient } from "@/utils/supabase/server";
+// import type { Database } from "@/types/database.types";
+// import BeforeLoginPage from "@/app/money-mindful/login/page";
 
-
-
-// // 認証状態の監視
-// export default async function signinPage() {
+// export default async function AuthLoginPage() {
 //   const supabase = await createClient<Database>();
-
-//   // セッションの取得
 //   const {
 //     data: { session },
 //   } = await supabase.auth.getSession();
 
-//   // 認証している場合、リダイレクト
 //   if (session) {
-//     redirect("/");
+//     redirect("/money-mindful/home");
 //   }
 
 //   return <BeforeLoginPage />;

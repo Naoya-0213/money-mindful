d9f7bd864129075cc4259bd924a62248441be943
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-19 10:03:13 +0900

    設定Email追加_7/19_10:03

--- src/app/money-mindful/setting/profile/change-email/page.tsx
+++ src/app/money-mindful/setting/profile/change-email/page.tsx
@@ -6,10 +6,78 @@ import Button from "@/app/components/button/Button";
 import DisplayField from "@/app/components/field/DisplayFeild";
 import FormField from "@/app/components/field/FormField";
 import SectionCard from "@/app/components/section-card/SectionCard";
-import Link from "next/link";
-import React from "react";
+import { createClient } from "@/utils/supabase/clients";
 
-const ChangeEmailPage = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import z from "zod";
+type Schema = z.infer<typeof schema>;
+
+// 入力データの検証ルールを定義
+const schema = z.object({
+  email: z.string().email({ message: "メールアドレスの形式ではありません。" }),
+});
+
+// メールアドレス変更
+const ChangeEmailPage = ({ email }: { email: string }) => {
+  const router = useRouter();
+  const supabase = createClient();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    // 初期値
+    defaultValues: { email: "" },
+    // 入力値の検証
+    resolver: zodResolver(schema),
+  });
+
+  // 送信
+  const onSubmit: SubmitHandler<Schema> = async (data) => {
+    setLoading(true);
+    setMessage("");
+
+    try {
+      // メールアドレス変更メールを送信
+      const { error: updateUserError } = await supabase.auth.updateUser(
+        { email: data.email },
+        { emailRedirectTo: `${location.origin}/auth/callback` },
+      );
+
+      // エラーチェック
+      if (updateUserError) {
+        setMessage("エラーが発生しました。" + updateUserError.message);
+        return;
+      }
+
+      setMessage("確認用のURLを記載したメールを送信しました。");
+
+      // ログアウト
+      const { error: signOutError } = await supabase.auth.signOut();
+
+      // エラーチェック
+      if (signOutError) {
+        setMessage("エラーが発生しました。" + signOutError.message);
+        return;
+      }
+
+      router.push("/auth/signin");
+    } catch (error) {
+      setMessage("エラーが発生しました。" + error);
+      return;
+    } finally {
+      setLoading(false);
+      router.refresh();
+    }
+  };
+
   return (
     <div className="mx-auto flex w-full max-w-[480px] min-w-[320px] flex-col gap-5 bg-[#F3F0EB]">
       <div className="flex w-full flex-col items-center gap-5 p-5">

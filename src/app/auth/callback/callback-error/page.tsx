69a9c559a7a9707c7985aa77d1af2852448efc29
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-21 13:03:38 +0900

    変更

--- src/app/auth/callback/callback-error/page.tsx
+++ src/app/auth/callback/callback-error/page.tsx
@@ -5,6 +5,8 @@ const CallbackErrorPage = () => {
     <div className="mx-auto flex w-full max-w-[480px] min-w-[320px] flex-col gap-5 bg-[#F3F0EB]">
       <div className="flex w-full flex-col items-center gap-5 p-5">
         <p>セッションが切れております。再度ログインしてください。</p>
+        <p>ログインの有効期限が切れているか、無効なリンクです。</p>
+        <p>お手数ですが、再度ログインし、もう一度同じ操作を行ってください。</p>
       </div>
     </div>
   );

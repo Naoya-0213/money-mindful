8f2df57563feee3261a10c32fc82488e271da9b2
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-21 14:19:13 +0900

    セッション切れ画面作成_20250721_14-19

--- src/app/auth/callback/callback-error/page.tsx
+++ src/app/auth/callback/callback-error/page.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
+import SessionErrorPage from "@/app/components/session-error/SessionErrorPage";
+
+// メールアドレス変更時、登録アドレスの送信されるリンククリック時の遷移先
+
 const CallbackErrorPage = () => {
-  return (
-    <div className="mx-auto flex w-full max-w-[480px] min-w-[320px] flex-col gap-5 bg-[#F3F0EB]">
-      <div className="flex w-full flex-col items-center gap-5 p-5">
-        <p>セッションが切れております。再度ログインしてください。</p>
-        <p>ログインの有効期限が切れているか、無効なリンクです。</p>
-        <p>お手数ですが、再度ログインし、もう一度同じ操作を行ってください。</p>
-      </div>
-    </div>
-  );
+  return <SessionErrorPage />;
 };
 
 export default CallbackErrorPage;
--- src/app/components/session-error/SessionErrorPage.tsx
+++ src/app/components/session-error/SessionErrorPage.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+
+import Button from "../button/Button";
+import SectionCard from "../section-card/SectionCard";
 
 const SessionErrorPage = () => {
   return (
-    <div>SessionError</div>
-  )
-}
+    <div className="mx-auto flex w-full max-w-[480px] min-w-[320px] flex-col gap-5 bg-[#F3F0EB]">
+      <div className="flex w-full flex-col items-center gap-5 p-5">
+        <SectionCard icon="/icon/error/warning.png" label="セッションエラー">
+          <div className="flex flex-col gap-5">
+            {/* 注意文 */}
+            <div className="flex flex-col gap-5 items-center font-semibold text-[#777777]">
+              <div className="flex flex-col items-center">
+                <p>セッションが切れております。</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <p>ログインの有効期限が切れているか、</p>
+                <p>無効なリンクです。</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <p>お手数ですが、</p>
+                <p>再度ログインしてください。</p>
+              </div>
+            </div>
+          </div>
 
-export default SessionErrorPage
+          {/* ログインボタン */}
+          <div className="flex justify-center">
+            <Button href="/money-mindful/login">ログインはこちら</Button>
+          </div>
+        </SectionCard>
+      </div>
+    </div>
+  );
+};
+
+export default SessionErrorPage;

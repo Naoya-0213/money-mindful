3027018cb1c6cc3f83096592ae87d5dd64babcde
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-22 17:48:08 +0900

    前画面でzustand保存のデータ取得を可能に変更_20250722_17:48

---
+++ src/app/money-mindful/ClientWrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+
+import useUserStore from "@/store/useUserStore";
+
+import { createClient } from "@/utils/supabase/clients";
+import { getCurrentUser } from "@/utils/supabase/getCurrentUser";
+
+// ===== 全体適用クライアント処理（zustadでの現情報の取得）=====
+
+export default function ClientWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createClient();
+  const setUser = useUserStore((state) => state.setUser);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getCurrentUser(supabase);
+      if (user) setUser(user);
+    };
+    fetchUser();
+  }, [setUser]);
+
+  return <>{children}</>;
+}

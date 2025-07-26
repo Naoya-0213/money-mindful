a5e9b835ea05b8a1068076a048f588488eaf5287
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-26 22:46:02 +0900

    目標編集用

--- src/app/money-mindful/setting/goal/edit/page.tsx
+++ src/app/money-mindful/setting/goal/edit/page.tsx
@@ -6,7 +6,6 @@ const GoalSettingEditPage = () => {
   return (
     <div className="mx-auto flex w-full max-w-[480px] min-w-[320px] flex-col gap-5 bg-[#F3F0EB]">
       <div className="flex w-full flex-col items-center gap-5 p-5">
-        {/* 目標初回設定画面をインポート */}
         <GoalSettingEdit />
       </div>
     </div>

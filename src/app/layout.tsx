fb36574d928b8c38c8b44dd3b7d230e26b73cb64
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-22 22:31:08 +0900

    ファイル修正_20250822_22-31

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import { Toaster } from "react-hot-toast";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import ClientUserSetter from "@/lib/ClientUserSetter";
+
+import { getCurrentUser } from "@/utils/supabase/getCurrentUser";
+
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import "./globals.css";
@@ -26,12 +30,17 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { session, profile } = await getCurrentUser();
+
   return (
     <html lang="jp">
       <body
         className={`${inter.className} flex min-h-screen flex-col items-center bg-white text-[#795549]`}
         style={{ minHeight: "100dvh" }}
       >
+        <ClientUserSetter session={session} profile={profile} />
+
+        {/* ヘッダー */}
         <Header />
         <main
           style={{
@@ -42,7 +51,7 @@ export default async function RootLayout({
         >
           {children}
 
-          {/* react-hot-toast */}
+          {/* トースト導入 */}
           <Toaster
             position="top-center"
             toastOptions={{
@@ -71,6 +80,8 @@ export default async function RootLayout({
             reverseOrder={false}
           />
         </main>
+
+        {/* フッター */}
         <Footer />
       </body>
     </html>
--- src/utils/supabase/getCurrentUser.ts
+++ src/utils/supabase/getCurrentUser.ts
@@ -1,33 +1,41 @@
+import { createClient } from "@/utils/supabase/clients";
+
 // ===== ユーザー情報の取得処理 =====
 // 📍Supabaseのユーザー情報とプロフィールを取得（サインイン後に使用）
 // email・名前・画像などを profiles テーブルから取得し、画面に渡すための関数
 // TODO これを全体layoutで使用し、簡単にユーザー取得できるようにすること。
 // TODO todo-list-nextjsを参考に更新すること
-import { createClient } from "@/utils/supabase/clients";
 
-export const getCurrentUser = async (
-  supabase: ReturnType<typeof createClient>,
-) => {
-  // supabaseデフォルトテーブルuserからemail・idを取得
-  const result = await supabase.auth.getUser();
+export const getCurrentUser = async () => {
+  const supabase = await createClient();
 
-  if (result.error || !result.data.user) {
-    console.error("ユーザー情報の取得に失敗");
-    return null;
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  let profile = null;
+
+  if (session) {
+    const { data: currentProfile } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("is", session.user.id)
+      .single();
+
+    profile = currentProfile;
+
+    // メールアドレス変更時に同期
+    if (currentProfile && currentProfile.email !== session.user.email) {
+      const { data: updatedProfile } = await supabase
+        .from("profiles")
+        .update({ email: session.user.email })
+        .match({ id: session.user.id })
+        .select("*")
+        .single();
+
+      profile = updatedProfile;
+    }
   }
 
-  // profiles テーブルから追加情報（name, image_urlなど）を取得
-  const user = result.data.user;
-
-  const { data: profile, error: profileError } = await supabase
-    .from("profiles")
-    .select("*") // emailも含まれているなら取得可能
-    .eq("id", user.id)
-    .single();
-
-  if (profileError || !profile) {
-    console.error("プロフィール情報の取得に失敗:", profileError.message);
-    return null;
-  }
-  return profile;
+  return { session, profile };
 };

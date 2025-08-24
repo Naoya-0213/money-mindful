a6e8a68015bf991d0114c83984757f5733281fa1
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-24 21:49:36 +0900

    css変更_20250824_21-49

--- src/app/components/loading/LoadingSpinner.tsx
+++ src/app/components/loading/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className="mt-20 flex justify-center">
+    <div className="m-auto flex justify-center">
       <div className="h-7 w-7 animate-spin rounded-full border-2 border-[#795549] border-t-transparent" />
     </div>
   );

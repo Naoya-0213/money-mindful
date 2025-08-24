688c0c19c3d941d20565ea8d78f6ddd0c4675a5f
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-24 14:33:20 +0900

    フォルダ分け整理_20250824_14-33

---
+++ src/app/(money-mindful)/page.tsx
@@ -0,0 +1,35 @@
+// "use server"
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+import { Database } from "@/types/database.types";
+
+import BeforeSignin from "../components/before-signin/BeforeSignin";
+
+// ===== トップページ（未ログイン時の初期画面） =====
+// 📍ルートパス（"/"）で使用
+// ログイン済みなら /money-mindful/home にリダイレクト、未ログインなら BeforeSignin を表示
+
+export default async function PageTop() {
+  const supabase = await createClient<Database>();
+
+  // セッションを取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return redirect("/auth/signin");
+  }
+
+  if (session) {
+    redirect("/home");
+  }
+
+  return (
+    <div>
+      <BeforeSignin />
+    </div>
+  );
+}
